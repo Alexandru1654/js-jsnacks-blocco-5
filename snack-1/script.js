d64264b19c9c3c2a6fbe296cff3f37b2205eefab
alexandru233847 <alexutualexino33@gmail.com>
2024-10-16 17:17:52 +0200

    Print in console

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,5 +1,8 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
-
+names.forEach(function (entry) {
+    console.log(entry);
+}
+)
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
